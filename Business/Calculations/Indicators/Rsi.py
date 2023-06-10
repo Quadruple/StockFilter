a4d46fb95a91042fbaec22c7ecf6c515140660a4
Atakan Ataman Atik <atakanatamana@gmail.com>
2023-06-11 00:17:13 +0300

    For loop in rsi calculation replaced with map for state removal

--- Business/Calculations/Indicators/Rsi.py
+++ Business/Calculations/Indicators/Rsi.py
@@ -6,16 +6,21 @@ def calculate_close_price_differences(close_prices: pd.Series) -> pd.Series:
     return close_prices.diff()
 
 
+def get_up_ewm_based_on_difference(difference: float) -> float:
+    if difference < 0:
+        return 0
+    return difference
+
+
+def get_down_ewm_based_on_difference(difference: float) -> float:
+    if difference < 0:
+        return difference
+    return 0
+
+
 def calculate_ewm(differences: pd.Series) -> tuple[pd.Series, pd.Series]:
-    up_ewm = []
-    down_ewm = []
-    for difference in differences:
-        if difference < 0:
-            up_ewm.append(0)
-            down_ewm.append(difference)
-        else:
-            up_ewm.append(difference)
-            down_ewm.append(0)
+    down_ewm = list(map(get_down_ewm_based_on_difference, differences))
+    up_ewm = list(map(get_up_ewm_based_on_difference, differences))
     return pd.Series(up_ewm).ewm(com=13, adjust=False).mean(), \
         pd.Series(down_ewm).abs().ewm(com=13, adjust=False).mean()
 
