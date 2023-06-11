f085b34f02abae8126f2cd26774f925dea5e10dd
Atakan Ataman Atik <atakanatamana@gmail.com>
2023-06-11 22:54:58 +0300

    Bollinger band indication implemented

--- Business/Calculations/Indicators/BollingerBands.py
+++ Business/Calculations/Indicators/BollingerBands.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from Business.Data.SignalTypes import SIGNAL_TYPES
 
 
 def get_upper_bollinger_band(close_prices: pd.Series, period: int, multiplier: int) -> pd.Series:
@@ -7,3 +8,12 @@ def get_upper_bollinger_band(close_prices: pd.Series, period: int, multiplier: i
 
 def get_lower_bollinger_band(close_prices: pd.Series, period: int, multiplier: int) -> pd.Series:
     return close_prices.rolling(period).mean() - close_prices.rolling(period).std() * multiplier
+
+
+def get_bollinger_indication(close_prices: pd.Series, upper_band: pd.Series, lower_band: pd.Series) -> SIGNAL_TYPES:
+    # TODO: Bollinger indication needs conditions for trend following.
+    if close_prices[-1] <= lower_band[-1]:
+        return SIGNAL_TYPES.BUY
+    if close_prices[-1] >= upper_band[-1]:
+        return SIGNAL_TYPES.SELL
+    return SIGNAL_TYPES.HOLD
