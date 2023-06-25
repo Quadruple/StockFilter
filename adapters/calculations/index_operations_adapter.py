e0c01218daa0c0d206e9ab49b3394877d6144693
Atakan Ataman Atik <atakanatamana@gmail.com>
2023-06-25 23:40:17 +0300

    Missed import statements are fixed for the correct format

--- adapters/calculations/index_operations_adapter.py
+++ adapters/calculations/index_operations_adapter.py
@@ -1,8 +1,8 @@
 """
 This module is an abstraction for web scraping and formatting between different data sources.
 """
-from drivers.calculations.IndexSearcher import scrape_stocks_from_trading_view
-from drivers.actions.StockFormatter import transform_stock_names_into_yahoo_format
+from drivers.calculations.index_searcher import scrape_stocks_from_trading_view
+from drivers.actions.stock_formatter import transform_stock_names_into_yahoo_format
 
 
 def scrape_component_names_under_index(page_source: str) -> list[str]:
